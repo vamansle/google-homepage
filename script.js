1cf15352222bc5360785332471413b7b5e1f1e2f
HoShin Kwag <hoshinva@gmail.com>
2017-05-04 02:20:59 -0400

    2nd commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	for(var j = 0; j < 4; j++){
+		for(var i = 0; i < 4; i++){
+			$('#container').append('<div class="squares"></div>');
+		}
+		$('#container').append('<br/>')
+	}
+	$('.squares').hover(function(){
+			
+	});
+});
